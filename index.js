ddbe6a8de1a3fe4111410c90c6b881138c783521
Agrima <er.agrima.malhotra@gmail.com>
2023-02-08 00:38:53 +0530

    feat: add express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const routes = require('./src/routes/app');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', routes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
