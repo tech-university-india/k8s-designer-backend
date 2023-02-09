cc02a9b01a26a4d2d91497755d7e2815d5449770
Agrima <er.agrima.malhotra@gmail.com>
2023-02-10 02:10:47 +0530

    feat: added filter fn in services

--- src/services/customer.services.js
+++ src/services/customer.services.js
@@ -3,11 +3,12 @@ const data = require('../../input.json');
 const getCustomersData = () => data.log;
 
 const getCustomerData = customerId => {
-  for (const record of data.log) {
+  return data.log.filter((record) => {
     if (record.customerId == customerId) {
       return record;
     }
-  }
+  });
 };
+
 
 module.exports = { getCustomersData, getCustomerData };
