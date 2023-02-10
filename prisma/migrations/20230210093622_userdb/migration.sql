a5f521542ae1469346491f549163df38517b1967
shashwath sk <shashukarkera01@gmail.com>
2023-02-10 18:09:04 +0530

    chore: create k8s database and create user table in it

---
+++ prisma/migrations/20230210093622_userdb/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "full_name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
