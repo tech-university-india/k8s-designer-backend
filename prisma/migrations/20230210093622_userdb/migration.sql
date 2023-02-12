34afcd8a1e89e00413e6da5003d208b6abb6a20d
shashwath sk <shashukarkera01@gmail.com>
2023-02-12 11:26:26 +0530

    fix: remove user_model migration and incoperate those changes in userdb migration

--- prisma/migrations/20230210093622_userdb/migration.sql
+++ prisma/migrations/20230210093622_userdb/migration.sql
@@ -3,8 +3,6 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "full_name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "salt" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
