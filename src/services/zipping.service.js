c1f8c089162c5d5c6844442a9c5777ae4d77ca5b
apoorvGit <apoorvmishra1000@gmail.com>
2023-02-16 23:31:06 +0530

    refactor: remove console logs

--- src/services/zipping.service.js
+++ src/services/zipping.service.js
@@ -4,47 +4,30 @@ const path = require('path');
 
 function zipFolder(folderPath, outputPath) {
   const outputFilePath = path.resolve(outputPath);
-
+    
   return new Promise((resolve, reject) => {
-    console.log(`Creating zip file for folder: ${folderPath}`);
     const resolvedFolderPath = path.resolve(folderPath);
+
     if (!fs.existsSync(resolvedFolderPath)) {
       reject(`Folder not found: ${resolvedFolderPath}`);
     }
+
     const folderName = path.parse(resolvedFolderPath).base;
+
     const output = fs.createWriteStream(outputFilePath);
 
     const archive = archiver('zip', {
       zlib: { level: 9 }
     });
 
-    archive.on('warning', (err) => {
-      if (err.code === 'ENOENT') {
-        console.warn(err);
-      } else {
-        reject(err);
-      }
-    });
-
-    archive.on('error', (err) => {
-      reject(err);
-    });
-
     archive.pipe(output);
-
     archive.directory(resolvedFolderPath, folderName);
-
     archive.finalize();
 
     output.on('close', () => {
-      const bytesWritten = output.bytesWritten;
-      console.log(`Archive written to ${outputFilePath} (${bytesWritten} bytes)`);
       resolve(outputFilePath);
     });
 
-    output.on('error', (err) => {
-      reject(err);
-    });
   });
 }
 
