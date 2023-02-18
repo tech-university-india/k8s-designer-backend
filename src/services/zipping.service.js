242bc44f30ab6e1fbb51f71c67f617f81fda8474
apoorvGit <apoorvmishra1000@gmail.com>
2023-02-18 14:08:42 +0530

    update: promisify the existence check of a folder

--- src/services/zipping.service.js
+++ src/services/zipping.service.js
@@ -1,33 +1,30 @@
-const archiver = require('archiver');
 const fs = require('fs');
 const path = require('path');
+const archive = require('../config/archiver.config.js');
+const {doesFolderExist}= require('../utility/folder.utility.js');
+
 
 function zipFolder(folderPath, outputPath) {
   const outputFilePath = path.resolve(outputPath);
     
   return new Promise((resolve, reject) => {
     const resolvedFolderPath = path.resolve(folderPath);
+    doesFolderExist(resolvedFolderPath).then(() => {
 
-    if (!fs.existsSync(resolvedFolderPath)) {
-      reject(`Folder not found: ${resolvedFolderPath}`);
-    }
+      const folderName = path.parse(resolvedFolderPath).base;
 
-    const folderName = path.parse(resolvedFolderPath).base;
+      const output = fs.createWriteStream(outputFilePath);
 
-    const output = fs.createWriteStream(outputFilePath);
+      archive.pipe(output);
+      archive.directory(resolvedFolderPath, folderName);
+      archive.finalize();
 
-    const archive = archiver('zip', {
-      zlib: { level: 9 }
+      output.on('close', () => {
+        resolve(outputFilePath);
+      });
+    }).catch((err) => {
+      reject(err);
     });
-
-    archive.pipe(output);
-    archive.directory(resolvedFolderPath, folderName);
-    archive.finalize();
-
-    output.on('close', () => {
-      resolve(outputFilePath);
-    });
-
   });
 }
 
