3c835dde145d239e3be5cffff512d7ebae3a0210
apoorvGit <apoorvmishra1000@gmail.com>
2023-02-19 01:24:54 +0530

    refactor: import folderUtility instead of directly importing doesFolderExists function

--- src/services/zipping.service.js
+++ src/services/zipping.service.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const archive = require('../config/archiver.config.js');
-const {doesFolderExist}= require('../utility/folder.utility.js');
+const folderUtility= require('../utility/folder.utility.js');
 
 
 function zipFolder(folderPath, outputPath) {
@@ -9,7 +9,7 @@ function zipFolder(folderPath, outputPath) {
     
   return new Promise((resolve, reject) => {
     const resolvedFolderPath = path.resolve(folderPath);
-    doesFolderExist(resolvedFolderPath).then(() => {
+    folderUtility.doesFolderExist(resolvedFolderPath).then(() => {
 
       const folderName = path.parse(resolvedFolderPath).base;
 
