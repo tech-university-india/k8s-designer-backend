b081f541b9d850dd5070b21781c611094a5ffe16
PreetinderSingh345 <preetindersingh072@gmail.com>
2023-02-18 22:02:32 +0530

    chore: update tests for docker compose generator

--- src/services/generators/docker-compose.js
+++ src/services/generators/docker-compose.js
@@ -1,5 +1,6 @@
 const mustache = require('mustache');
 const fs = require('fs').promises;
+const path = require('path');
 const serviceTemplatePaths = require('../../constants/serviceTemplatePaths');
 
 const dockerComposeGenerator = async (projectId, serviceType, config) => {
@@ -10,7 +11,7 @@ const dockerComposeGenerator = async (projectId, serviceType, config) => {
             throw new Error(`Invalid service type: ${serviceType}`);
         }
 
-        const projectDir = __dirname + `/../../constants/tmp/${projectId}`;
+        const projectDir = path.join(__dirname, `../../constants/tmp/${projectId}`);
 
         try {
             await fs.access(projectDir);
@@ -18,14 +19,14 @@ const dockerComposeGenerator = async (projectId, serviceType, config) => {
             await fs.mkdir(projectDir);
         }
 
-        const dockerComposePath = projectDir + '/docker-compose.yaml';
+        const dockerComposePath = path.join(projectDir, 'docker-compose.yaml');
 
-        const template = await fs.readFile(templatePath, 'utf8');
-        const dokcerComposeFile = mustache.render(template, config);
-        await fs.writeFile(dockerComposePath, dokcerComposeFile, 'utf-8');
+        const template = await fs.readFile(templatePath, 'utf-8');
+        const dockerComposeFile = mustache.render(template, config);
+        await fs.writeFile(dockerComposePath, dockerComposeFile, 'utf-8');
     } catch (err) {
         console.error(err.message);
     }
 }
 
-module.exports = { dockerComposeGenerator }
+module.exports = dockerComposeGenerator;
--- tests/services/generators/docker-compose.test.js
+++ tests/services/generators/docker-compose.test.js
@@ -1,23 +1,105 @@
-const fs = require('fs');
-const {dockerComposeGenerator} = require('../../../src/services/generators/docker-compose');
+const fs = require('fs').promises;
+const path = require('path');
+const serviceTemplatePaths = require('../../../src/constants/serviceTemplatePaths');
+const dockerComposeGenerator = require('../../../src/services/generators/docker-compose.js');
+
+const projectId = 1;
+
+const serviceType = 'FRONTEND';
+const invalidServiceType = 'INVALID_SERVICE_TYPE';
+
+const config = {
+    name: 'frontend-application',
+    port: 3000,
+    internalPort: 3000,
+    environment: [
+        { name: 'BACKEND_API_URL', value: 'http://backend-application/8080' },
+        { name: 'DEBUG', value: true }
+    ],
+    replicas: 3
+}
 
 describe('dockerComposeGenerator', () => {
-    const mockFrontendConfig = {
-        name: 'frontend-application',
-        port: 3000,
-        internalPort: 3000,
-        environment: [
-            {name: "BACKEND_API_URL", value: "http://backend-application:8080"},
-            {name: 'DEBUG', value: true}
-        ],
-        replicas: 3
-    };
+    const mockAccess = jest.fn();
+    const mockMkdir = jest.fn();
+    const mockReadFile = jest.fn();
+    const mockWriteFile = jest.fn();
 
-    const expectedDockerComposeFile = fs.readFileSync(__dirname+'/../../mocks/docker-compose/mock-frontend.yaml', 'utf-8');
+    beforeAll(() => {
+        jest.spyOn(fs, 'access').mockImplementation(mockAccess);
+        jest.spyOn(fs, 'mkdir').mockImplementation(mockMkdir);
+        jest.spyOn(fs, 'readFile').mockImplementation(mockReadFile);
+        jest.spyOn(fs, 'writeFile').mockImplementation(mockWriteFile);
+    });
 
-    it('should generate the expected Docker Compose file from the given configuration', () => {
-        const generatedDockerComposeFile = dockerComposeGenerator(mockFrontendConfig);
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 
-        expect(generatedDockerComposeFile).toEqual(expectedDockerComposeFile);
+    afterAll(() => {
+        jest.restoreAllMocks();
+    });
+
+    it('should generate a docker-compose.yaml file', async () => {
+        const templatePath = serviceTemplatePaths[serviceType];
+        const projectDir = path.join(__dirname, `../../../src/constants/tmp/${projectId}`);
+        const dockerComposePath = path.join(projectDir, 'docker-compose.yaml');
+
+        const template = 
+        
+            `version: '3'
+            services:
+            frontend:
+                name: "{{name}}"
+                ports:
+                - "{{port}}:{{internalPort}}"
+                environment:
+            {{#environment}}
+                {{name}}: "{{{value}}}"
+            {{/environment}}
+                deploy:
+                replicas: {{replicas}}
+            `;
+
+        const expectedDockerComposeFile = 
+        
+            `version: '3'
+            services:
+            frontend:
+                name: "frontend-application"
+                ports:
+                - "3000:3000"
+                environment:
+                BACKEND_API_URL: "http://backend-application/8080"
+                DEBUG: "true"
+                deploy:
+                replicas: 3
+            `;
+
+        mockReadFile.mockResolvedValue(template);
+
+        await dockerComposeGenerator(projectId, serviceType, config);
+
+        expect(mockAccess).toHaveBeenCalledWith(projectDir);
+        
+        if(mockMkdir.mock.calls.length === 1) {
+            expect(mockMkdir).toHaveBeenCalledWith(projectDir);
+        }
+
+        expect(mockReadFile).toHaveBeenCalledWith(templatePath, 'utf-8');
+        expect(mockWriteFile).toHaveBeenCalledWith(dockerComposePath, expectedDockerComposeFile, 'utf-8');
+    });
+
+    it('should throw an error if the service type is invalid', async () => {
+        try {
+            await expect(dockerComposeGenerator(projectId, invalidServiceType, config));
+        } catch(err) {
+            expect(err.message).toBe('Invalid service type: INVALID_SERVICE_TYPE');
+        }
+
+        expect(mockAccess).not.toHaveBeenCalled();
+        expect(mockMkdir).not.toHaveBeenCalled();
+        expect(mockReadFile).not.toHaveBeenCalled();
+        expect(mockWriteFile).not.toHaveBeenCalled();
     });
 });
