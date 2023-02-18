29ac4b01a048d88539ad23e2ee474450d6c30562
apoorvGit <apoorvmishra1000@gmail.com>
2023-02-19 01:32:40 +0530

    update:  use spyOn instead of actually creating zip and deleting it in success case

--- tests/services/zipping.service.spec.js
+++ tests/services/zipping.service.spec.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 const path = require('path');
 const { zipFolder } = require('../../src/services/zipping.service.js');
+const archive = require('../../src/config/archiver.config.js');
+const folderUtility= require('../../src/utility/folder.utility.js');
+jest.useFakeTimers();
+
+
 describe('Zipping Service', ()=>{
   afterEach(() => {
     jest.clearAllMocks();
   });
-
+  
   it('should reject with an error if folder does not exists', async () => {
     const mockFolderPath = path.join(__dirname, 'nonExistingFolder');
     const mockOutputPath = path.join(__dirname, 'mockFolder.zip');
@@ -13,27 +18,33 @@ describe('Zipping Service', ()=>{
   });
 
   it('should reject with an error if there is a problem creating the zip file', async ()=>{
+    jest.spyOn(folderUtility, 'doesFolderExist').mockResolvedValue(true);
     jest.spyOn(fs, 'createWriteStream').mockImplementation(()=>{
       throw new Error('WriteStream error');
     });
-    await expect(zipFolder('./tests', './output.zip')).rejects.toThrow('WriteStream error');
+    await expect(zipFolder('./inputDir', './output.zip')).rejects.toThrow('WriteStream error');
     fs.createWriteStream.mockRestore();
   });
 
   it('should zip a folder and return the archive details', async () => {
-    const mockFolderPath = path.join(__dirname, 'mockFolder');
-    const mockOutputPath = path.join(__dirname, 'mockFolder.zip');
-    
-    fs.mkdirSync(mockFolderPath);
-    fs.writeFileSync(path.join(mockFolderPath, 'file1.txt'), 'file1 content');
-    fs.writeFileSync(path.join(mockFolderPath, 'file2.txt'), 'file2 content');
-    
-    const outputZipPath= await zipFolder('tests/services/mockFolder', 'tests/services/mockFolder.zip');
+    const mockOutputPath = path.join(__dirname, 'output.zip');
 
+    jest.spyOn(folderUtility, 'doesFolderExist').mockResolvedValue(true); 
+    jest.spyOn(archive, 'pipe').mockResolvedValue('Success'); 
+    jest.spyOn(archive, 'finalize').mockResolvedValue(true); 
+
+    const mReadStream = {
+      on: jest.fn().mockImplementation(function (event, handler) {
+        if(event === 'close'){
+          handler();
+        }
+        return this;
+      }),
+    };
+    jest.spyOn(fs, 'createWriteStream').mockReturnValueOnce(mReadStream);
+    
+    const outputZipPath= await zipFolder('./input', 'tests/services/output.zip');
     expect(outputZipPath).toBe(mockOutputPath);
-
-    fs.rmSync(outputZipPath);
-    fs.rmSync(mockFolderPath, { recursive: true });
   });
 
 });
