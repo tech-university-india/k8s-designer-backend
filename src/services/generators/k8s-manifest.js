8457423366ebddc37318cc7a01ffd7e5344c3d59
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-03-04 22:12:29 +0530

    fix: error handling in k8s-manifest generator file

--- src/services/generators/k8s-manifest.js
+++ src/services/generators/k8s-manifest.js
@@ -1,10 +1,25 @@
 const path = require("path");
+const fs = require("fs").promises;
 const { exec } = require("child_process");
 const { OUTPUT_PATH } = require("../../constants/app.constants");
+const ProjectDirectoryNotFoundException = require("../../exceptions/ProjectDirectoryNotFoundException");
 
 const k8sManifestGenerator = async (projectId, dockerComposePath) => {
   try {
+    try {
+      await fs.stat(dockerComposePath);
+    } catch (err) {
+      throw new ProjectDirectoryNotFoundException(dockerComposePath);
+    }
+
     const projectDir = path.join(OUTPUT_PATH, projectId.toString());
+
+    try {
+      await fs.stat(projectDir);
+    } catch (err) {
+      throw new ProjectDirectoryNotFoundException(projectDir);
+    }
+
     const k8sManifestPath = path.join(projectDir, "k8s-manifest.yaml");
 
     exec(
