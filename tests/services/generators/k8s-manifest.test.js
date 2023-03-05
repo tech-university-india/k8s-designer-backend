15e289a71f850be904221d7c074f9ae305180ee4
dilipsingh27 <dilipsinghrathore803@gmail.com>
2023-03-05 20:36:02 +0530

    fix: linting in the k8s-manifest file

--- tests/services/generators/k8s-manifest.test.js
+++ tests/services/generators/k8s-manifest.test.js
@@ -1,84 +1,73 @@
-const { spawn } = require('child_process');
-const { stderr } = require('process');
+const { spawn } = require("child_process");
+const { stderr } = require("process");
 
-const k8sManifestGenerator  = require('../../../src/services/generators/k8s-manifest');
+const k8sManifestGenerator = require("../../../src/services/generators/k8s-manifest");
 
 let mockSpawn = {};
 let mockWriteStream = {};
 
-jest.mock('fs', () => ({
-    createWriteStream: () => mockWriteStream,
-  }),
-);
+jest.mock("fs", () => ({
+  createWriteStream: () => mockWriteStream,
+}));
 
-jest.mock('child_process', () => ({
-    spawn: () => mockSpawn,
-  }),
-);
+jest.mock("child_process", () => ({
+  spawn: () => mockSpawn,
+}));
 
-describe('k8sManifestGenerator', () => {
+describe("k8sManifestGenerator", () => {
+  it("should generate a k8s-manifest.yaml file", async () => {
+    mockSpawn = {
+      on: jest.fn().mockImplementation((event, callback) => {
+        if (event === "close") {
+          callback(0);
+        }
+      }),
+      stdout: {
+        pipe: jest.fn(),
+      },
+    };
 
-    it('should generate a k8s-manifest.yaml file', async () => {
-      mockSpawn = {
-          on: jest.fn().mockImplementation((event, callback) => {
-            if (event === 'close') {
-              callback(0);
-            }
-          }),
-          stdout: {
-              pipe: jest.fn()
-          },
-      };
+    const code = await k8sManifestGenerator(1);
+    expect(code).toBe(0);
+  });
 
-      const code = await k8sManifestGenerator(1);
-      expect(code).toBe(0);
-    });
+  it("should throw error when the k8s-manifest is not generated", async () => {
+    mockSpawn = {
+      on: jest.fn().mockImplementation((event, callback) => {
+        if (event === "close") {
+          callback(1);
+        }
+      }),
+      stdout: {
+        pipe: jest.fn(),
+      },
+    };
 
+    try {
+      await k8sManifestGenerator(1);
+    } catch (e) {
+      expect(e).toBe(1);
+    }
+  });
 
-    it('should throw error when the k8s-manifest is not generated', async () => {
-      mockSpawn = {
-          on: jest.fn().mockImplementation((event, callback) => {
-            if (event === 'close') {
-              callback(1);
-            }
-          }),
-          stdout: {
-              pipe: jest.fn()
-          },
-      };
+  it("should throw an error if docker-compose.yaml file does not exist", async () => {
+    mockSpawn = {
+      on: jest.fn().mockImplementation((event, callback) => {
+        if (event === "close") {
+          callback(1);
+        }
+      }),
+      stdout: {
+        pipe: jest.fn().mockImplementation(() => {
+          throw new Error("error");
+        }),
+      },
+    };
 
-      try {
-          await k8sManifestGenerator(1);
-      }
-      catch (e) {
-          expect(e).toBe(1);
-      }
-
-    });
-
-
-    it('should throw an error if docker-compose.yaml file does not exist', async () => {
-      mockSpawn = {
-          on: jest.fn().mockImplementation((event, callback) => {
-            if (event === 'close') {
-              callback(1);
-            }
-          }),
-          stdout: {
-              pipe: jest.fn().mockImplementation(()=> {
-                  throw new Error('error');
-              })
-          }
-      };
-
-      try {
-          await k8sManifestGenerator(1);
-      }
-      catch (e) {
-          expect(e.message).toBe('error') 
-      }
-
-    });
-
+    try {
+      await k8sManifestGenerator(1);
+    } catch (e) {
+      expect(e.message).toBe("error");
+    }
+  });
 });
-
