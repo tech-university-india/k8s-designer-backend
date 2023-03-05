ef77632c1efda688b932773cb2169980accf4420
Preetinder Singh <preetindersingh072@gmail.com>
2023-03-05 16:41:09 +0530

    feat: update docker compose generator for backend configurations (#31)

--- tests/services/generators/docker-compose.test.js
+++ tests/services/generators/docker-compose.test.js
@@ -1,40 +1,54 @@
-const fs = require('fs').promises;
-const path = require('path');
+const fs = require("fs").promises;
+const path = require("path");
 const {
   TEMPLATE_PATH,
   UTF8_ENCODING,
   OUTPUT_PATH,
-} = require('../../../src/constants/app.constants');
-const dockerComposeGenerator = require('../../../src/services/generators/docker-compose.js');
-const InvalidServiceTypeException = require('../../../src/exceptions/InvalidServiceTypeException');
-const ProjectDirectoryNotFoundException = require('../../../src/exceptions/ProjectDirectoryNotFoundException');
+} = require("../../../src/constants/app.constants");
+const dockerComposeGenerator = require("../../../src/services/generators/docker-compose.js");
+const InvalidServiceTypeException = require("../../../src/exceptions/InvalidServiceTypeException");
+const ProjectDirectoryNotFoundException = require("../../../src/exceptions/ProjectDirectoryNotFoundException");
 
 const projectId = 1;
-const invalidProjectId = '999';
 
-const serviceType = 'FRONTEND';
-const invalidServiceType = 'INVALID_SERVICE_TYPE';
+const invalidProjectId = "999";
+const invalidServiceType = "INVALID_SERVICE_TYPE";
 
-const config = {
-  name: 'frontend-application',
+const frontendConfig = {
+  name: "frontend-application",
   port: 3000,
   internalPort: 3000,
   environment: [
-    { name: 'BACKEND_API_URL', value: 'http://backend-application/8080' },
-    { name: 'DEBUG', value: true },
+    { name: "BACKEND_API_URL", value: "http://backend-application/8080" },
+    { name: "DEBUG", value: true },
   ],
   replicas: 3,
 };
 
-describe('dockerComposeGenerator', () => {
+const backendConfig = {
+  name: "backend-application",
+  image: "nginx:latest",
+  port: 8080,
+  internalPort: 8080,
+  environment: [
+    {
+      name: "DATABASE_URL",
+      value: "postgres://user:password@localhost:5432/mydatabase",
+    },
+    { name: "JWT_SECRET", value: "randomjwtsecret" },
+  ],
+  replicas: 3,
+};
+
+describe("dockerComposeGenerator", () => {
   const mockStat = jest.fn();
   const mockReadFile = jest.fn();
   const mockWriteFile = jest.fn();
 
   beforeAll(() => {
-    jest.spyOn(fs, 'stat').mockImplementation(mockStat);
-    jest.spyOn(fs, 'readFile').mockImplementation(mockReadFile);
-    jest.spyOn(fs, 'writeFile').mockImplementation(mockWriteFile);
+    jest.spyOn(fs, "stat").mockImplementation(mockStat);
+    jest.spyOn(fs, "readFile").mockImplementation(mockReadFile);
+    jest.spyOn(fs, "writeFile").mockImplementation(mockWriteFile);
   });
 
   afterEach(() => {
@@ -45,10 +59,12 @@ describe('dockerComposeGenerator', () => {
     jest.restoreAllMocks();
   });
 
-  it('should generate a docker-compose.yaml file', async () => {
+  it("should generate a docker-compose.yaml file for frontend configurations", async () => {
+    const serviceType = "FRONTEND";
+
     const templatePath = TEMPLATE_PATH[serviceType];
     const projectDir = path.join(OUTPUT_PATH, projectId.toString());
-    const dockerComposePath = path.join(projectDir, 'docker-compose.yaml');
+    const dockerComposePath = path.join(projectDir, "docker-compose.yaml");
 
     const template = `version: '3'
             services:
@@ -82,7 +98,7 @@ describe('dockerComposeGenerator', () => {
 
     mockReadFile.mockResolvedValue(template);
 
-    await dockerComposeGenerator(projectId, serviceType, config);
+    await dockerComposeGenerator(projectId, serviceType, frontendConfig);
 
     expect(mockStat).toHaveBeenCalledWith(projectDir);
     expect(mockReadFile).toHaveBeenCalledWith(templatePath, UTF8_ENCODING);
@@ -93,9 +109,59 @@ describe('dockerComposeGenerator', () => {
     );
   });
 
-  it('should throw an InvalidServiceTypeError if the service type is invalid', async () => {
+  it("should generate a docker-compose.yaml file for standalone configurations", async () => {
+    const serviceType = "BACKEND";
+
+    const templatePath = TEMPLATE_PATH[serviceType];
+    const projectDir = path.join(OUTPUT_PATH, projectId.toString());
+    const dockerComposePath = path.join(projectDir, "docker-compose.yaml");
+
+    const template = `version: '3'
+            services:
+            {{name}}:
+                image: {{image}}
+                ports:
+                - "{{port}}:{{internalPort}}"
+                environment:
+            {{#environment}}
+                {{name}}: "{{{value}}}"
+            {{/environment}}
+                deploy:
+                replicas: {{replicas}}
+            `;
+
+    const expectedDockerComposeFile = `version: '3'
+            services:
+            backend-application:
+                image: nginx:latest
+                ports:
+                - "8080:8080"
+                environment:
+                DATABASE_URL: "postgres://user:password@localhost:5432/mydatabase"
+                JWT_SECRET: "randomjwtsecret"
+                deploy:
+                replicas: 3
+            `;
+
+    const mockedStats = { size: 1024 };
+    mockStat.mockResolvedValue(mockedStats);
+
+    mockReadFile.mockResolvedValue(template);
+
+    await dockerComposeGenerator(projectId, serviceType, backendConfig);
+
+    expect(mockStat).toHaveBeenCalledWith(projectDir);
+    expect(mockReadFile).toHaveBeenCalledWith(templatePath, UTF8_ENCODING);
+    expect(mockWriteFile).toHaveBeenCalledWith(
+      dockerComposePath,
+      expectedDockerComposeFile,
+      UTF8_ENCODING
+    );
+  });
+
+  it("should throw an InvalidServiceTypeError if the service type is invalid", async () => {
     await expect(
-      dockerComposeGenerator(projectId, invalidServiceType, config)
+      dockerComposeGenerator(projectId, invalidServiceType, frontendConfig)
     ).rejects.toThrow(InvalidServiceTypeException);
 
     expect(mockStat).not.toHaveBeenCalled();
@@ -103,13 +169,15 @@ describe('dockerComposeGenerator', () => {
     expect(mockWriteFile).not.toHaveBeenCalled();
   });
 
-  it('should throw a ProjectDirectoryNotFoundError if the project directory does not exist', async () => {
+  it("should throw a ProjectDirectoryNotFoundError if the project directory does not exist", async () => {
+    const serviceType = "FRONTEND";
+
     mockStat.mockRejectedValue(
       new ProjectDirectoryNotFoundException(invalidProjectId)
     );
 
     await expect(
-      dockerComposeGenerator(invalidProjectId, serviceType, config)
+      dockerComposeGenerator(invalidProjectId, serviceType, frontendConfig)
     ).rejects.toThrow(ProjectDirectoryNotFoundException);
 
     expect(mockReadFile).not.toHaveBeenCalled();
