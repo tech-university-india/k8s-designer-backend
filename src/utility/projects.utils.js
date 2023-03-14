cbb75e1b1d8ec52414da9f344b8dd69d974f9d8a
apoorvGit <apoorvmishra1000@gmail.com>
2023-03-14 11:48:31 +0530

    feat: restructure the flow of record insertion in tables and add functionality to handle backend as one of the service

--- src/utility/projects.utils.js
+++ src/utility/projects.utils.js
@@ -2,49 +2,68 @@
 const projectServiceConfigRepository = require('../repositories/projectServiceConfig.repositories');
 const envVariablesRepository = require('../repositories/envVariables.repositories');
 const frontendServiceRepository = require('../repositories/frontendService.repositories');
-const projectRepository = require('../repositories/project.repositories');
-
+const backendServiceRepository = require('../repositories/backendService.repositories');
 
 const repositoryServiceObj = {
-    FrontEnd: async (service)=>{
+    FrontEnd: async (service, projectId)=>{
         const {service_type,configurations,customEnv} = service;
-        const frontendServicesResult = await frontendServiceRepository.create(
-      
-            {reactVersion:configurations.reactVersion, 
-                numberOfReplicas:configurations.numberOfReplicas, 
-                name:configurations.name, 
-                port:configurations.port}
-      
-        );
-        const frontendServicesId =  frontendServicesResult.id;
-        // eslint-disable-next-line no-unused-vars
-        const envVariablesResult = await envVariablesRepository.create(
-            {field:customEnv.field,
-                value:customEnv.value, 
-                frontendServicesId
-            });
-
-        const projectResult = await projectRepository.create(
-      
-            {userId:'c2ed1118-a016-4140-9c19-dd7eee774071'}
-        
-        );
-      
-        const projectId =  projectResult.id;
-
         const projectServiceConfigResult = await projectServiceConfigRepository.create(
             {
                 serviceType:service_type,
-                serviceId:frontendServicesId,
                 projectId:projectId 
             }
       
         );
-          
-        return projectServiceConfigResult;
+        const serviceId= projectServiceConfigResult.id;
+        
+        await frontendServiceRepository.create(
+      
+            {
+                numberOfReplicas:configurations.numberOfReplicas, 
+                name:configurations.name, 
+                port:configurations.port,
+                serviceId:serviceId
+            }
+      
+        );
+        await envVariablesRepository.create(
+            {field:customEnv.field,
+                value:customEnv.value, 
+                serviceId: serviceId,
+            });
+
+        return serviceId;
     },
 
-    BackEnd: ()=>{},
+    BackEnd: async (service, projectId)=>{
+        const {service_type,configurations,customEnv} = service;
+        const projectServiceConfigResult = await projectServiceConfigRepository.create(
+            {
+                serviceType:service_type,
+                projectId:projectId 
+            }
+      
+        );
+        const serviceId= projectServiceConfigResult.id;
+        
+        await backendServiceRepository.create(
+      
+            {
+                numberOfReplicas:configurations.numberOfReplicas, 
+                name:configurations.name, 
+                port:configurations.port,
+                serviceId:serviceId
+            }
+      
+        );
+        await envVariablesRepository.create(
+            {field:customEnv.field,
+                value:customEnv.value, 
+                serviceId: serviceId,
+            });  
+          
+        return serviceId;
+    },
     Database: ()=>{}
 };
 
